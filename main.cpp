cfe504f1db758ffb8c618fde99e7ef188f5b0ce8
Muhammad Azeem <mailmeatazeem@gmail.com>
2024-06-16 14:14:25 +0500

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,48 @@
+// Code
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) 
+    {
+        ListNode* temp = new ListNode();
+        ListNode* ans = temp;
+
+        while(list1 && list2)
+        {
+            if(list1->val > list2->val)
+            {
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            else
+            {
+                temp->next = list1;
+                list1 = list1->next;
+            }
+            temp = temp->next;
+        }
+        while(list1)
+        {
+            temp->next = list1;
+            list1 = list1->next;
+            temp = temp->next;
+        }
+        while(list2)
+        {
+            temp->next = list2;
+            list2 = list2->next;
+            temp = temp->next;
+        }
+        return ans->next;
+    }
+};
